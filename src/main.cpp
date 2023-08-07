bf43258f38cbc3d62ba205aa4ce36adeee634ab9
Puppy <honor.ston3@gmail.com>
2023-08-07 06:15:25 -0400

    - Build folder was ignored - Added cpphttplib - Tested cpphttplib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "cpphttplib.hpp"
 
 int main(int argc, char **argv) {
 	std::cout << "Hello Program" << '\n';
+
+	// HTTP
+	httplib::Client cli("http://localhost:4567");
+
+	auto res = cli.Get("/api/facts");
+	std::cout<<res->status<<std::endl;
+	std::cout<<res->body<<std::endl;
 	return 0;
 };
